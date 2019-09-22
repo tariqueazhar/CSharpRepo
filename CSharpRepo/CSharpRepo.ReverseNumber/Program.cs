b8332d92cf5527b4105d0c1040e370d0ce07f143
Tarique Azhar <Tarique.Azhar@neudesic.com>
2019-09-22 19:35:01 +0530

    Csharprepo

---
+++ CSharpRepo/CSharpRepo.ReverseNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpRepo.ReverseNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpRepo/CSharpRepo.ReverseString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpRepo.ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
